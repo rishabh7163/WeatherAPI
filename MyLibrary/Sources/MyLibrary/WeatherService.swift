d7e14aa0dc9bb96f262c4c4905429782f705970d
Rishabh7163 <rishabhsri96@gmail.com>
2022-11-15 13:08:52 -0800

    adding weather API files to git

---
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -0,0 +1,38 @@
+import Alamofire
+
+public protocol WeatherService {
+    func getTemperature() async throws -> Int
+}
+
+enum urls : String {
+    case mockServiceUrl = "http://172.29.0.2:5000"
+}
+
+class WeatherServiceImpl: WeatherService {
+    
+    let url = urls.mockServiceUrl.rawValue
+
+    func getTemperature() async throws -> Int {
+        return try await withCheckedThrowingContinuation { continuation in
+            AF.request(url, method: .get).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
+                switch response.result {
+                case let .success(weather):
+                    let temperature = weather.main.temp
+                    let temperatureAsInteger = Int(temperature)
+                    continuation.resume(with: .success(temperatureAsInteger))
+
+                case let .failure(error):
+                    continuation.resume(with: .failure(error))
+                }
+            }
+        }
+    }
+}
+
+public struct Weather: Decodable {
+    let main: Main
+
+    struct Main: Decodable {
+        let temp: Double
+    }
+}
